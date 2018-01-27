e18768892a62f2fd79e678264c6efb5db2abc7c9
GreatHackTun <35867659+GreatHackTun@users.noreply.github.com>
2018-01-27 18:12:44 +0600

    Add files via upload

---
+++ DButil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nurkhan
+ */
+public class DButil {
+    
+    
+    
+}
