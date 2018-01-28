335398655092dd5b6e3f84f9049cf283bae2a3f8
GreatHackTun <35867659+GreatHackTun@users.noreply.github.com>
2018-01-28 08:07:09 +0600

    Add files via upload

--- DButil.java
+++ DButil.java
@@ -1,9 +1,11 @@
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Util;
+
 
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
@@ -12,12 +14,53 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Nurkhan
- */
 public class DButil {
+    static final String user = "root";
+    static final String pass = "";
+    static final String DBname = "greetgo";
+    static PreparedStatement ps = null;
+ 
+    Connection connection;
+    Statement statement;
+    DButil(){
+	try{
+	    Class.forName("com.mysql.jdbc.Driver");
+            connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DBname, user, pass);
+            statement = connection.createStatement();
+        }catch(Exception e){
+            System.out.println("Cant connect to DB");
+            }
+        }
     
+    public ResultSet getData(String query){
+	ResultSet rs = null;
+	try{
+	    rs = statement.executeQuery(query);
+	}catch(Exception e){
+	    System.out.println(e);
+	}
+	return rs;
+    }
     
-    
+public int check(String param1,String param2){
+            ResultSet rs = getData("SELECT * FROM user");
+            
+          try {
+              while(rs.next()){
+                  String login = rs.getString("login");
+                  String password = rs.getString("password");
+                  if(login.equals(param1) && password.equals(param2)){
+		      int id = rs.getInt("id");
+                      System.out.println("This is your "+id);
+		      return id;
+                  }
+              }
+          } catch (SQLException ex) {
+              System.out.println(ex);
+          }
+            
+            return 0;
+        }
 }
+    
+
--- LoginServlet.java
+++ LoginServlet.java
@@ -4,34 +4,39 @@
  * and open the template in the editor.
  */
 
+
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Nurkhan
  */
 public class LoginServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
+    DButil util = new DButil();
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
 	response.setContentType("text/html;charset=UTF-8");
-	try (PrintWriter out = response.getWriter()) {
+	
 	    /* TODO output your page here. You may use following sample code. */
-	}
+	    String login = request.getParameter("login");
+	    String password = request.getParameter("password");
+	    HttpSession session = request.getSession(true);
+	    int check = util.check(login, password);
+	    if(check>0){
+               request.getRequestDispatcher("schedule.jsp").forward(request, response);
+	    }
+	    else{
+		response.sendRedirect("sorry.jsp");
+	    }
+	    
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
